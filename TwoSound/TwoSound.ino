0725a169c9d1543875a5b933483584c12782d80c
Gary Jinghua Hicks <h_gary@outlook.com>
2018-04-16 13:23:41 -0400

    Add files via upload

--- TwoSound/TwoSound.ino
+++ TwoSound/TwoSound.ino
@@ -1,45 +1,41 @@
-int trig = 5;
+int trig = 5; //In this section we are defining what the pins on the arduino are being used for
 int echo = 6;
 int trig2 = 9;
 int echo2 = 10;
 
 int ledPin = 13;
-bool lightStatus = false;
 
+bool lightStatus = false; //Here we are defining variables that will be needed later on 
 double theTime;
 double distance;
-
 double theTime2;
 double distance2;
 
-//unsigned long start;
-
 void setup() {
   // put your setup code here, to run once:
-  pinMode(trig, OUTPUT); 
+  pinMode(trig, OUTPUT); //Defining how I want to use the pins on the arduino (either input or output)
   pinMode(echo, INPUT);
   pinMode(trig2, OUTPUT); 
   pinMode(echo2, INPUT);
   pinMode(ledPin, OUTPUT); 
-  Serial.begin(9600); 
+  Serial.begin(9600); //Initializing the rate at which I'll communicate with my computer
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+void loop() { //The code in this section will run over and over again
+
   //First Sensor
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
+  digitalWrite(trig, LOW); //Setting the trigger pin to not send anything. Note: For explanation of how the science works, see report.
+  delayMicroseconds(2); //Wait
 
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
+  digitalWrite(trig, HIGH); //Send waves form the trig pin
+  delayMicroseconds(10); //It does this for 10 microseconds
+  digitalWrite(trig, LOW); //We turn it off again
 
-  theTime = pulseIn(echo, HIGH);
-  distance = theTime*0.0343*0.394/2;
+  theTime = pulseIn(echo, HIGH); //Gathering the amount of time from the echo pin
+  distance = theTime*0.0343/2; //Calculating distance
 
   //Second Sensor
-  digitalWrite(trig2, LOW);
+  digitalWrite(trig2, LOW); //Same as first sensor, but using the second sensor instead
   delayMicroseconds(2);
 
   digitalWrite(trig2, HIGH);
@@ -47,36 +43,29 @@ void loop() {
   digitalWrite(trig2, LOW);
 
   theTime2 = pulseIn(echo2, HIGH);
-  distance2 = theTime2*0.0343*0.394/2;
+  distance2 = theTime2*0.0343/2;
   
-  //Print
-  //Serial.print("Sensor 1: ");
-  Serial.println(distance);
-  //Serial.print("Sensor 2: ");
+  //Printing
+  Serial.println(distance); //Printing the two distances
   Serial.println(distance2);
 
-  if ((Serial.available() > 0) && (Serial.available() < 2)) {
-    if (lightStatus == false) {
-      digitalWrite(ledPin, HIGH);
-      lightStatus = true;
+  if (Serial.available() == 1) { //Code in this section will only run when I've sent information from my python program
+    if (lightStatus == false) { //If the light is turned off, code in this if will run
+      digitalWrite(ledPin, HIGH); //Turning the led on
+      lightStatus = true; //Keeping track of whether the light is on or off. 
     }
-    else{
-      digitalWrite(ledPin, LOW);
-      lightStatus = false;
+    else{ // If the light is turned on, this will run
+      digitalWrite(ledPin, LOW); //Turning led off
+      lightStatus = false; //Keeping track of whether light is on or off. In essence, we should flip between this section and the section above each time since the lgiht status changes, thus creating blinking.
     }
-
-    //digitalWrite(ledPin, HIGH);
   }
-  else if (Serial.available() == 2) {
-    digitalWrite(ledPin, LOW);
-    lightStatus = false;
-    Serial.read();
+  else if (Serial.available() == 2) { //Code will only run once there are two values sent from my python program
+    digitalWrite(ledPin, LOW); //Turning light off
+    lightStatus = false; //Storing the light status
+    Serial.read(); //Clearing the information in the serial by reading the two values
     Serial.read();
   }
   
-  delay(1000);
-  /*unsigned long end = micros();
-  unsigned long delta = end - start;
-  Serial.println(delta);
-  start = micros();*/
+  delay(1000); //Waits 1 second before going back to the top of the loop and running again
+
 }
